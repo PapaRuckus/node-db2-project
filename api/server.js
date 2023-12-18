8fc7d7209ce3a9537eef87280a00b8814c964aa4
Calvin K <calvinknaub@gmail.com>
2023-12-18 14:33:38 -0800

    wip router

--- api/server.js
+++ api/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const carRouter = require("./cars/cars-router");
 
 const server = express();
 
-// DO YOUR MAGIC
 server.use(express.json());
+server.use("/api/cars", carRouter);
 
 server.use("*", (req, res, next) => {
   next({
